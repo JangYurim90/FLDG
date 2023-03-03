b6d4f273ab156d5332d9446d17c815c8d0c4e94b
yrjang <aemijang812@gmail.com>
2023-03-03 21:12:12 +0900

    [ADD] options

--- src/options.py
+++ src/options.py
@@ -3,8 +3,84 @@ import argparse
 def args_parser():
     parser = argparse.ArgumentParser()
 
+    # dataset
+    parser.add_argument('--dataset-dir', type=str, default='dataset',
+                        help='home directory to dataset')
+    parser.add_argument('--dataset', type=str, default='pacs',
+                        help='dataset name')
+    parser.add_argument('--sources', type=str, nargs='*',
+                        help='domains for train')
+    parser.add_argument('--targets', type=str, nargs='*',
+                        help='domains for test')
+
+    # save dir
+    parser.add_argument('--save-dir', type=str, default='checkpoint',
+                        help='home directory to save model')
+    parser.add_argument('--method', type=str, default='sagnet',
+                        help='method name')
+
+    # data loader
+    parser.add_argument('--workers', type=int, default=4,
+                        help='number of workers')
+    parser.add_argument('--batch-size', type=int, default=32,
+                        help='batch size for each source domain')
+    parser.add_argument('--input-size', type=int, default=256,
+                        help='input image size')
+    parser.add_argument('--crop-size', type=int, default=224,
+                        help='crop image size')
+    parser.add_argument('--colorjitter', type=float, default=0.4,
+                        help='color jittering')
+
+    # model
+    parser.add_argument('--arch', type=str, default='sag_resnet',
+                        help='network archiecture')
+    parser.add_argument('--depth', type=str, default='18',
+                        help='depth of network')
+    parser.add_argument('--drop', type=float, default=0.5,
+                        help='dropout ratio')
+
+    # sagnet
+    parser.add_argument('--sagnet', action='store_true', default=False,
+                        help='use sagnet')
+    parser.add_argument('--style-stage', type=int, default=3,
+                        help='stage to extract style features {1, 2, 3, 4}')
+    parser.add_argument('--w-adv', type=float, default=0.1,
+                        help='weight for adversarial loss')
+
+    # training policy
+    parser.add_argument('--from-sketch', action='store_true', default=False,
+                        help='training from scratch')
+    parser.add_argument('--lr', type=float, default=0.004,
+                        help='initial learning rate')
+    parser.add_argument('--weight-decay', type=float, default=1e-4,
+                        help='weight decay')
+    parser.add_argument('--iterations', type=int, default=2000,
+                        help='number of training iterations')
+    parser.add_argument('--scheduler', type=str, default='cosine',
+                        help='learning rate scheduler {step, cosine}')
+    parser.add_argument('--milestones', type=int, nargs='+', default=[1000, 1500],
+                        help='milestones to decay learning rate (for step scheduler)')
+    parser.add_argument('--gamma', type=float, default=0.1,
+                        help='gamma to decay learning rate')
+    parser.add_argument('--momentum', type=float, default=0.9,
+                        help='SGD momentum')
+    parser.add_argument('--clip-adv', type=float, default=0.1,
+                        help='grad clipping for adversarial loss')
+
+    # etc
+    parser.add_argument('--seed', type=int, default=-1,
+                        help='random seed')
+    parser.add_argument('--log-interval', type=int, default=10,
+                        help='iterations for logging training status')
+    parser.add_argument('--log-test-interval', type=int, default=10,
+                        help='iterations for logging test status')
+    parser.add_argument('--test-interval', type=int, default=100,
+                        help='iterations for test')
+    parser.add_argument('-g', '--gpu-id', type=str, default='0',
+                        help='gpu id')
+
     # federated arguments
-    parser.add_argument('--epochs',type = int, default = 1000,
+    parser.add_argument('--global-ep',type = int, default = 1000,
                         help="number of rounds of training")
     parser.add_argument('--num_users', type=int, default=100,
                         help="number of users : K")
@@ -14,11 +90,8 @@ def args_parser():
                         help="the number of local epochs: E")
     parser.add_argument('--local_bs', type=int, default=10,
                         help="local batch size : B")
-    parser.add_argument('--lr', type=float, default=0.01,
-                        help='learning rate')
-    parser.add_argument('--momentum', type=float, default=0.5,
-                        help='SGD momentum (default: 0.5)')
 
+'''
     # model arguments
     parser.add_argument('--model', type=str, default='cnn', help='model name')
     parser.add_argument('--kernel_num', type=int, default=9,
@@ -36,15 +109,17 @@ def args_parser():
     parser.add_argument('--max_pool', type=str, default='True',
                         help="Whether use max pooling rather than \
                            strided convolutions")
+     '''
+
 
     # other arguments
-    parser.add_argument('--dataset', type=str, default='mnist', help="name \
+    #parser.add_argument('--dataset', type=str, default='mnist', help="name \
                            of dataset")
-    parser.add_argument('--num_classes', type=int, default=10, help="number \
+    #parser.add_argument('--num_classes', type=int, default=10, help="number \
                            of classes")
-    parser.add_argument('--gpu', default=1, help="To use cuda, set \
+    #parser.add_argument('--gpu', default=1, help="To use cuda, set \
                            to a specific GPU ID. Default set to use CPU.")
-    parser.add_argument('--optimizer', type=str, default='sgd', help="type \
+    #parser.add_argument('--optimizer', type=str, default='sgd', help="type \
                            of optimizer")
     parser.add_argument('--iid', type=int, default=0,
                         help='Default set to IID. Set to 0 for non-IID.')
@@ -53,7 +128,8 @@ def args_parser():
                            non-i.i.d setting (use 0 for equal splits)')
     parser.add_argument('--stopping_rounds', type=int, default=10,
                         help='rounds of early stopping')
-    parser.add_argument('--verbose', type=int, default=1, help='verbose')
-    parser.add_argument('--seed', type=int, default=1, help='random seed')
+    #parser.add_argument('--verbose', type=int, default=1, help='verbose')
+    #parser.add_argument('--seed', type=int, default=1, help='random seed')
+
     args = parser.parse_args()
     return args
--- src/utils.py
+++ src/utils.py
@@ -27,6 +27,86 @@ def get_dataset(args):
 
     return train_dataset, test_dataset, user_groups
 
+
+def init_loader():
+    global loader_srcs, loader_vals, loader_tgts
+    global num_classes
+
+    # Set transforms
+    stats = ((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+
+    trans_list = []
+    trans_list.append(transforms.RandomResizedCrop(args.crop_size, scale=(0.5, 1)))
+    if args.colorjitter:
+        trans_list.append(transforms.ColorJitter(*[args.colorjitter] * 4))
+    trans_list.append(transforms.RandomHorizontalFlip())
+    trans_list.append(transforms.ToTensor())
+    trans_list.append(transforms.Normalize(*stats))
+
+    train_transform = transforms.Compose(trans_list)
+    test_transform = transforms.Compose([
+        transforms.Resize(args.input_size),
+        transforms.CenterCrop(args.crop_size),
+        transforms.ToTensor(),
+        transforms.Normalize(*stats)])
+
+    # Set datasets
+    if args.dataset == 'pacs':
+        from data.pacs import PACS
+        image_dir = os.path.join(args.dataset_dir, args.dataset, 'images', 'kfold')
+        split_dir = os.path.join(args.dataset_dir, args.dataset, 'splits')
+
+        print('--- Training ---')
+        dataset_srcs = [PACS(image_dir,
+                             split_dir,
+                             domain=domain,
+                             split='train',
+                             transform=train_transform)
+                        for domain in args.sources]
+        print('--- Validation ---')
+        dataset_vals = [PACS(image_dir,
+                             split_dir,
+                             domain=domain,
+                             split='crossval',
+                             transform=test_transform)
+                        for domain in args.sources]
+        print('--- Test ---')
+        dataset_tgts = [PACS(image_dir,
+                             split_dir,
+                             domain=domain,
+                             split='test',
+                             transform=test_transform)
+                        for domain in args.targets]
+        num_classes = 7
+    else:
+        raise NotImplementedError('Unknown dataset: {}'.format(args.dataset))
+
+    # Set loaders
+    kwargs = {'num_workers': args.workers, 'pin_memory': True}
+    loader_srcs = [torch.utils.data.DataLoader(
+        dataset,
+        batch_size=args.batch_size,
+        shuffle=True,
+        drop_last=True,
+        **kwargs)
+        for dataset in dataset_srcs]
+    loader_vals = [torch.utils.data.DataLoader(
+        dataset,
+        batch_size=int(args.batch_size * 4),
+        shuffle=False,
+        drop_last=False,
+        **kwargs)
+        for dataset in dataset_vals]
+    loader_tgts = [torch.utils.data.DataLoader(
+        dataset_tgt,
+        batch_size=int(args.batch_size * 4),
+        shuffle=False,
+        drop_last=False,
+        **kwargs)
+        for dataset_tgt in dataset_tgts]
+
+
+
 def average_weights(w):
     w_avg = copy.deepcopy(w[0]) #w[0] 깊은 복사
     for key in w_avg.keys():
